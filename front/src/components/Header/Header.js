89e8e95db088d1e2cb4db6c1bca65ebf512c65c2
Matt Coy <fluffytoycoy@gmail.com>
2019-08-05 17:29:57 -0500

    init commit

---
+++ front/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Header.scss';
+
+function Header(){
+
+    return (
+      <div id="header">
+        header
+      </div>
+
+    );
+}
+
+export default Header;
