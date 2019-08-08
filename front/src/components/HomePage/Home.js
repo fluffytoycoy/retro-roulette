d57fe1805403b9158dee924265d25303678bf6b8
Matt Coy <fluffytoycoy@gmail.com>
2019-08-07 20:41:57 -0500

    firefox fix

--- front/src/components/HomePage/Home.js
+++ front/src/components/HomePage/Home.js
@@ -67,7 +67,7 @@ scrollTo() {
               </div>
               <div className="col">
                 <div>
-                  <h1>Place your bet</h1>
+                  <h1>Place your bets</h1>
                   <p>Placing your bet will let you select which console or consoles will show up in the roulette.</p>
                   <p>In addtion you can cross off genres you don't want such as Sports, RPGs, Racing, ect.</p>
                   <p>The cards are in your hands, Ante up.</p>
