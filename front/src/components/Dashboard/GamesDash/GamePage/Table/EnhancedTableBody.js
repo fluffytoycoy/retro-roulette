b30be9d61e41bb1b058b38746589f80b1fbb5715
Matt Coy <fluffytoycoy@gmail.com>
2019-08-19 07:24:26 -0700

    created console and tab dashboards

--- front/src/components/Dashboard/GamesDash/GamePage/Table/EnhancedTableBody.js
+++ front/src/components/Dashboard/GamesDash/GamePage/Table/EnhancedTableBody.js
@@ -1,8 +1,8 @@
 import React from 'react';
 //import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import EnhancedTableHead from './Table/EnhancedTableHead';
-import EnhancedToolbar from './Table/EnhancedToolbar';
+import EnhancedTableHead from './EnhancedTableHead';
+import EnhancedToolbar from './EnhancedToolbar';
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
